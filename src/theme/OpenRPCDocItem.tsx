1d250ebae6f869b931773f663452be9b1bc66fae
Zachary Belford <belfordz66@gmail.com>
2023-04-20 15:25:39 -0700

    fixup styling and loop over methods to produce sidebar items

--- src/theme/OpenRPCDocItem.tsx
+++ src/theme/OpenRPCDocItem.tsx
@@ -3,40 +3,47 @@ const DocSidebar = require("@theme/DocSidebar").default;
 import {
   Sidebar,
 } from "@docusaurus/plugin-content-docs/src/sidebars/types";
-
 import Layout from '@theme/Layout';
 const MDXContent = require('@theme/MDXContent').default;
+import {MethodObject} from '@open-rpc/meta-schema';
 
 export default function Hello(props: any) {
   const sidebar: Sidebar = [
     {
       label: "OpenRPC",
       description: "OpenRPC is a specification for machine-readable JSON-RPC services. It allows for automatic code generation, documentation, and discovery.",
-      items: [
-        {
+      items: props.openrpcDocument.methods.map((method: MethodObject) => {
+        return {
           type: "link",
-          label: "OpenRPC Potato",
-          href: "/api-playground#foo",
-        },
-      ],
+          label: method.name,
+          href: `/api-playground#${method.name}`,
+        }
+      }),
       type: "category",
       collapsed: false,
       collapsible: true,
     }
-  ]
-  console.log(props)
+  ];
 
   return (
     <Layout>
-      <div>
+      <div className="docPage_node_modules-@docusaurus-theme-classic-lib-theme-DocPage-Layout-styles-module">
         <aside className="theme-doc-sidebar-container docSidebarContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocPage-Layout-Sidebar-styles-module">
-          <DocSidebar path={props.route.path} sidebar={sidebar}/>
+          <div className="sidebarViewport_node_modules-@docusaurus-theme-classic-lib-theme-DocPage-Layout-Sidebar-styles-module">
+            <DocSidebar path={props.route.path} sidebar={sidebar}/>
+          </div>
         </aside>
+
         <main className="docMainContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocPage-Layout-Main-styles-module">
-            <MDXContent>
-              <props.openrpcMarkdown></props.openrpcMarkdown>
-            </MDXContent>
-            {/* </MDXProvider> */}
+          <div className="container padding-top--md padding-bottom--lg">
+            <div className="row">
+              <div className="col col--12">
+                <MDXContent>
+                  <props.openrpcMarkdown></props.openrpcMarkdown>
+                </MDXContent>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </Layout>
