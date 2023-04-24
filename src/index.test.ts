714f262a51dba59b3e8f365df1f70974dbde430d
Zachary Belford <belfordz66@gmail.com>
2023-04-24 11:18:59 -0700

    lint fix

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,32 +1,26 @@
-import { LoadContext } from "@docusaurus/types";
-import DocusaurusOpenRPC from ".";
+import { LoadContext } from '@docusaurus/types';
 
-describe("docusaurus openrpc plugin", () => {
+import docusaurusOpenRpc from '.';
 
-  describe("files to watch method ", () => {
-    it("returns nothing when the openrpcDocument option is a url", () => {
-      const plugin = DocusaurusOpenRPC(
-        {} as LoadContext,
-        {
-          openrpcDocument: 'https://anything.example',
-          path: 'foo'
-        }
-      );
+describe('docusaurus openrpc plugin', () => {
+  describe('files to watch method', () => {
+    it('returns nothing when the openrpcDocument option is a url', () => {
+      const plugin = docusaurusOpenRpc({} as LoadContext, {
+        openrpcDocument: 'https://anything.example',
+        path: 'foo',
+      });
       const getPathsToWatch = plugin.getPathsToWatch as () => string[];
 
       const paths = getPathsToWatch();
       expect(paths).toStrictEqual([]);
     });
 
-    it("returns the same path as openrpcDocument if its not a url", () => {
+    it('returns the same path as openrpcDocument if its not a url', () => {
       const pathToOpenRPCDocument = '../foo/bar.baz';
-      const plugin = DocusaurusOpenRPC(
-        {} as LoadContext,
-        {
-          openrpcDocument: pathToOpenRPCDocument,
-          path: 'foo'
-        }
-      );
+      const plugin = docusaurusOpenRpc({} as LoadContext, {
+        openrpcDocument: pathToOpenRPCDocument,
+        path: 'foo',
+      });
       const getPathsToWatch = plugin.getPathsToWatch as () => string[];
       const paths = getPathsToWatch();
       expect(paths).toStrictEqual([pathToOpenRPCDocument]);
